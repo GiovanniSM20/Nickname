d5579c7caec64d5a23b27fd064faef693ae7bd20
Gabriel Carvalho Gama <gabriel.carvalhogama@hotmail.com>
2016-06-21 17:26:02 -0300

    Home - 4/5
    Home com 4/5 completo

---
+++ public_html/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8" />
+    <title>Nickname - Home</title>
+
+    <link rel="stylesheet" href="lib/css/materialize.min.css" />
+    <!--<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">-->
+    <link rel="stylesheet" href="lib/css/home.css" />
+  </head>
+  <body>
+    <header>
+      <img src="lib/images/nickname.png" />
+      <nav>
+        <ul>
+          <li><a href='/'>Home</a></li>
+          <li><a href='shop'>Shop</a></li>
+          <li><a href='contact'>Contato</a></li>
+          <li><a href='about'>Sobre</a></li>
+
+        </ul>
+      </nav>
+    </header>
+
+    <script src="lib/js/jquery.js"></script>
+    <script src="lib/js/home.js"></script>
+  </body>
+</html>
