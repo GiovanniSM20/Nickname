09cb1fc48bd459e42a9306921d5fe1b211d073e0
Gabriel Carvalho Gama <gabriel.carvalhogama@hotmail.com>
2016-07-22 16:50:06 -0300

    Nickname 1.0.9

--- public_html/shop.php
+++ public_html/shop.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Nickname - Shop</title>
-	<link rel="shortcut icon" href="lib/images/favicon.jpeg" />
+	  <link rel="shortcut icon" href="lib/images/favicon.jpeg" />
 
     <link rel="stylesheet" href="lib/css/materialize.min.css" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -16,7 +16,151 @@
     <?=get_header() ?>
     <div id="main">
       <div id="content">
-        
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+          <a href="#" class="no_effect">
+            <div class="flipper">
+              <div class="front">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/small_image/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-back.jpg" />
+              </div>
+              <div class="back">
+                <img src="https://www.hoodbyair.com/media/catalog/product/cache/1/falling_model/350x/17f82f742ffe127f42dca9de82fb58b1/m/e/mesh-front.jpg" />
+              </div>
+            </div>
+          </a>
+        </div>
+
       </div>
     </div>
     <script src="lib/js/jquery.js"></script>
